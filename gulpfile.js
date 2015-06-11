cab6367899d478d3d43d96213f38d83c04b643a0
Yadex205 <yadex205@outlook.jp>
2015-06-11 17:28:01 +0900

    use gul-server-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,39 @@
 // Require
 var gulp = require("gulp"),
 	compass = require("gulp-compass"),
+	ejs = require("gulp-ejs"),
 	plumber = require("gulp-plumber"),
 	notify = require("gulp-notify"),
-	exec = require("child_process").exec;
+	server = require("gulp-server-livereload"),
+	exec = require("gulp-exec");
 
 // Filepath
-var sass_files = "./sass/**/*.scss";
+var ejs_compile_files = ["./ejs/**/*.ejs", '!' + "./ejs/**/_*.ejs"],
+	ejs_watch_files = "./ejs/**/*.ejs",
+	sass_files = "./sass/**/*.scss";
 
-gulp.task("default");
-gulp.task("build", ["sass"]);
-gulp.task("test", ["build"], function () {
-	/*
-	exec("bundle exec jekyll serve --port 8080 --watch", function(error, stdout, stderr) {
-		console.log(stdout);
-		console.log(stderr);
-		if (error !== null) {
-			notify.onError("Error: <%= error %>");
-		}
-	});
-	*/
-	gulp.watch(sass_files, ["sass"]);
-});
+gulp.task("default", ["build"]);
 gulp.task("init", function () {
 	exec("bundle install --path vendor/bundle");
 });
+gulp.task("build", ["ejs", "sass"]);
+gulp.task("test", ["build"], function () {
+	gulp.watch(ejs_watch_files, ["ejs"]);
+	gulp.watch(sass_files, ["sass"]);
+	gulp.src("./_site")
+		.pipe(server({
+			livereload: true,
+			port: 8080,
+			host: "localhost"
+		}));
+});
 
+
+gulp.task("ejs", function () {
+	gulp.src(ejs_compile_files)
+		.pipe(ejs())
+		.pipe(gulp.dest("./_layouts"));
+});
 gulp.task("sass", function () {
 	gulp.src(sass_files)
 		.pipe(plumber({
@@ -35,5 +43,7 @@ gulp.task("sass", function () {
 			config_file: "config.rb",
 			sass: "sass",
 			css: "css"
-		}));
+		}))
+		.pipe(exec("bundle exec jekyll build"))
+		.pipe(exec.reporter());
 });
